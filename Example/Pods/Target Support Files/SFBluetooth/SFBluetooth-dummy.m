2cd6da7bafefb4e65e30b3a5bc389477b41fd0f0
hsf <hsf89757@gmail.com>
2024-08-02 15:40:15 +0800

    新建项目

---
+++ Example/Pods/Target Support Files/SFBluetooth/SFBluetooth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SFBluetooth : NSObject
+@end
+@implementation PodsDummy_SFBluetooth
+@end
---
+++ Example/SFBluetooth/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SFBluetooth
+//
+//  Created by hsf89757 on 08/02/2024.
+//  Copyright (c) 2024 hsf89757. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
